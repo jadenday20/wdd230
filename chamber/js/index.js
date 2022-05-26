0786553f2315ed484b0563468cc80458cbabd08b
jadenday20 <day18007@byui.edu>
2022-05-26 09:41:21 -0600

    made some basic changes to the hero and socials

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -12,7 +12,7 @@ let year = d.getFullYear();
 
 document.getElementById("last_updated").textContent = `Last Updated: ${new Date(document.lastModified).toLocaleDateString()} at ${new Date().getHours(document.lastModified)}:${new Date().getMinutes(document.lastModified)}:${new Date().getSeconds(document.lastModified)}`;
 
-document.getElementById("today").textContent = `Current Date: ${day}, ${date} ${month} ${year}`;
+document.getElementById("today").textContent = `${day}, ${date} ${month} ${year}`;
 
 document.querySelector(".copyright_year").innerHTML = "&copy;" + new Date().getFullYear();
 
