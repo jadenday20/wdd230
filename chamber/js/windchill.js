c0b9861fd9da31a4d8d18c4b53622fec4cc5b6e6
jadenday20 <day18007@byui.edu>
2022-06-06 10:52:04 -0600

    fixed some bugs

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -3,7 +3,7 @@ document.getElementById("wind-chill").innerHTML = "N/A"
 function calculate_chill() {
     t = document.getElementById("temp").value;
     s = document.getElementById("speed").value;
-    if (t <=50 || s > 3.0) {
+    if (t <=50 && s > 3.0) {
         windchill = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s **0.16
         document.getElementById("wind-chill").innerHTML = `${Math.round(windchill*10)/10}°F`
         console.log("windchill")
@@ -16,6 +16,17 @@ function calculate_chill() {
 
 document.getElementById("temp").addEventListener("keyup", calculate_chill);
 document.getElementById("speed").addEventListener("keyup", calculate_chill);
-document.getElementById("speed").addEventListener("keyup", function(){
-    console.log("hello")
+
+
+document.getElementById("temp").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("temp").blur()
+        document.getElementById("speed").focus()
+    }
+});
+
+document.getElementById("speed").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("speed").blur()
+    }
 });
