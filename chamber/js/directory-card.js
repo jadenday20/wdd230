7e9f5fb7dcc4e6fcc3ad8dbade1d1e4dbd275a77
jadenday20 <day18007@byui.edu>
2022-06-28 22:56:45 -0600

    added the directory

---
+++ chamber/js/directory-card.js
@@ -0,0 +1,45 @@
+const requestURL = 'data/directory-cards.json';
+const cards = document.querySelector('.cards');
+
+function displayBusinesses(business) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let url = document.createElement('p');
+    let membership = document.createElement('p');
+    
+    h2.textContent = String(business.name);
+    address.textContent = `Address: ${(business.address)}`;
+    phone.textContent = `Phone: ${(business.phone)}`;
+    url.textContent = `Web URL: ${(business.url)}`;
+    membership.textContent = `Membership: ${(business.membership)}`;
+
+    // Set image attributes
+    logo.setAttribute('src', `images/logos/${business.image}`);
+    logo.setAttribute('alt', `logo for ${business.name}`);
+    logo.setAttribute('loading', 'lazy');  
+    
+    card.appendChild(h2);
+    card.appendChild(address)
+    card.appendChild(phone)
+    card.appendChild(url)
+    card.appendChild(membership)
+    card.appendChild(logo);
+    
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const businesses = jsonObject['businesses'];
+        businesses.forEach(displayBusinesses);
+    });
